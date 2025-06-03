8b237f4054ab7ca9ad3db01491f00357ec290cc8
areum0505 <areum05055@gmail.com>
2025-06-03 23:04:13 +0900

    feat: 카드 덱 클래스

---
+++ app/chance_card/deck.py
@@ -0,0 +1,55 @@
+import random
+from typing import List, Optional
+
+from app.chance_card.abstract import ChanceCard
+
+from app.chance_card.attack import *
+from app.chance_card.defence import *
+from app.chance_card.event import *
+
+
+class ChanceCardDeck:
+    def __init__(self):
+        self._deck = self.initial_cards()
+        self._discard_deck = []
+        random.shuffle(self._deck)
+
+    def initial_cards(self) -> list[ChanceCard]:
+        return [
+            AlienInvasionCard(),
+            BlackoutCard(),
+            CityChangeCard(),
+            CityDonationCard(),
+            EarthquakeCard(),
+            ForceTaxCard(),
+            ForcedCitySaleCard(),
+            InfectiousDiseaseCard(),
+            YellowDustCard(),
+            AngelCard(),
+            DiscountCouponCard(),
+            IslandEscapeCard(),
+            ShieldCard(),
+            TollFreeCard(),
+            DesertIslandMoveCard(),
+            FestivalCard(),
+            StartMoveCard(),
+            TaxPenaltyCard(),
+            WorldTravelCard()
+        ]
+
+    def draw(self) -> Optional[ChanceCard]:
+        if not self._deck:
+            print("찬스 카드가 모두 소진되었습니다. 덱을 다시 섞습니다.")
+            self.reset()
+        return self._deck.pop() if self._deck else None
+
+    def reset(self):
+        self._deck = self._discard_deck[:]
+        self._discard_deck = []
+        random.shuffle(self._deck)
+
+    def remaining_count(self) -> int:
+        return len(self._deck)
+
+    def discard(self, card: ChanceCard):
+        self._discard_deck.append(card)
